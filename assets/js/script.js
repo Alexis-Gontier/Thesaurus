f6289b38b8e319ee1b49a803bbe6a2c260704a2e
Alexis G <alexis.gontier03@gmail.com>
2024-04-08 11:49:04 +0200

    bientot fini

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// word of the day
+let word_of_the_day = document.querySelector('.word_of_the_day')
+let button_bot_open = document.querySelector('#open')
+let button_bot_close = document.querySelector('#close')
+
+button_bot_open.addEventListener('click', () => {
+
+    word_of_the_day.style.display = 'flex'
+})
+
+button_bot_close.addEventListener('click', () => {
+
+    word_of_the_day.style.display = 'none'
+})
